1cf676be5606f5ed22d310cface4075479b1f203
holding-1-at-a-time <rromerojr1@gmail.com>
2024-08-04 08:42:34 -0500

    "Updated .gitignore to include .env.local, refactored app/layout.tsx to add UserButton, removed app/page.tsx, and made minor changes to components/ui/card.tsx"

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,12 @@
+import { SignUp } from "@clerk/nextjs";
+import React from 'react';
+
+const page = () => {
+    return (
+        <div className="flex-center glassmorphism-auth-w-full h-screen">
+            <SignUp />
+        </div>
+    );
+};
+
+export default page;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Manrope } from "next/font/google";
 import ConvexClerkProvider from "../providers/ConvexClerkProvider";
+import { UserButton } from "clerk/nextjs";
 
 const manrope = Manrope({ subsets: ["latin"] });
 
@@ -10,12 +11,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ConvexClerkProvider>
-      <html lang="en">
-        <body className={`${manrope.className}`}>
-          {children}
-        </body>
-      </html>
-    </ConvexClerkProvider>
+    <header style={{ display: "flex", justifyContent: "space-between", padding: 20 }}>
+
+      <ConvexClerkProvider>
+        <UserButton />
+        <html lang="en">
+          <body className={`${manrope.className}`}>
+            {children}
+          </body>
+        </html>
+      </ConvexClerkProvider>
+    </header> 
   );
 }

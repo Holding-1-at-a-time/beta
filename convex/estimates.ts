b414ed65a27ffd37af14a4c1c3f21b4c6c3dda85
holding-1-at-a-time <rromerojr1@gmail.com>
2024-08-04 19:18:46 -0500

    Changes to be committed: 	modified:   convex/estimates.ts

--- convex/estimates.ts
+++ convex/estimates.ts
@@ -2,7 +2,7 @@
 import { query, mutation } from "./_generated/server";
 import { v } from "convex/values";
 
-export const list = query({
+export const listEstimates = query({
     args: {},
     handler: async (ctx) => {
         return await ctx.db.query("estimates").collect();

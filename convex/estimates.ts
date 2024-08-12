5a341c9a7a4382a424b48b9990c87a99b75f8782
holding-1-at-a-time <rromerojr1@gmail.com>
2024-08-12 15:29:23 -0500

    blank

--- convex/estimates.ts
+++ convex/estimates.ts
@@ -86,6 +86,6 @@ export const createEstimate = mutation({
                 createdAt: new Date().toISOString(),
             });
             return estimateId;
-        },
-    },
-})
+        }
+    }
+});
